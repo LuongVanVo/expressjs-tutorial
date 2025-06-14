f7f243498053b9ea64a4cd093178da9d94ef7422
vanvo299 <luongvanvo29@gmail.com>
2025-06-14 15:13:57 +0700

    Express JS #8 - DELETE Requests

--- src/index.mjs
+++ src/index.mjs
@@ -113,3 +113,17 @@ app.patch('/api/users/:id', (req, res) => {
     console.log(mockUsers);
     return res.status(200).send(mockUsers);
 })
+
+// DELETE Request
+app.delete('/api/users/:id', (req, res) => {
+    const {
+        params: { id },
+    } = req;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) return res.sendStatus(400);
+    const findUserIndex = mockUsers.findIndex(user => user.id === parsedId);
+    if (findUserIndex === -1) return res.sendStatus(404);
+    mockUsers.splice(findUserIndex, 1); // 1 la so luong phan tu can cat bo
+    console.log(mockUsers);
+    return res.sendStatus(200)
+})
