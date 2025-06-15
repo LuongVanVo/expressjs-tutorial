875cd6a230681fcc4d819d735a8ff61e43edcbe0
vanvo299 <luongvanvo29@gmail.com>
2025-06-15 21:45:49 +0700

    Express JS #12 - Cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,18 @@
 import express from "express";
-import routes from './routes/index.mjs';
+import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 app.use(express.json()); // Middleware to parse JSON bodies
-
-app.use(routes);
+app.use(cookieParser("helloworld")); // Middleware to parse cookies, with a secret for signed cookies
+app.use(routes); 
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.status(201).send({ msg: "Welcome to the Home Page!" });
+  res.cookie('hello', 'world', { maxAge: 10000, signed: true });
+  res.status(201).send({ msg: "Welcome to the Home Page!" });
 });
 
 app.listen(PORT, () => {
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -4,18 +4,24 @@ const router = Router();
 
 // Endpoint all products
 router.get("/api/products", (req, res) => {
-  res.send([
-    { id: 123, name: "chicken breast", price: 12.99 },
-    { id: 456, name: "broccoli", price: 3.99 },
-    { id: 789, name: "rice", price: 1.99 },
-    { id: 101, name: "olive oil", price: 5.49 },
-    { id: 102, name: "salt", price: 0.99 },
-    { id: 103, name: "pepper", price: 1.49 },
-    { id: 104, name: "garlic", price: 0.79 },
-    { id: 105, name: "onion", price: 0.89 },
-    { id: 106, name: "spinach", price: 2.49 },
-    { id: 107, name: "tomato", price: 0.99 },
-  ]);
+  console.log(req.headers.cookie);
+  console.log(req.cookies); // lấy cookie không có chữ ký
+  console.log(req.signedCookies.hello); // lấy cookie có chữ ký, 
+  if (req.signedCookies.hello && req.signedCookies.hello === "world") {
+    res.send([
+      { id: 123, name: "chicken breast", price: 12.99 },
+      { id: 456, name: "broccoli", price: 3.99 },
+      { id: 789, name: "rice", price: 1.99 },
+      { id: 101, name: "olive oil", price: 5.49 },
+      { id: 102, name: "salt", price: 0.99 },
+      { id: 103, name: "pepper", price: 1.49 },
+      { id: 104, name: "garlic", price: 0.79 },
+      { id: 105, name: "onion", price: 0.89 },
+      { id: 106, name: "spinach", price: 2.49 },
+      { id: 107, name: "tomato", price: 0.99 },
+    ]);
+  }
+  return res.status(403).send({ msg: "Sorry.You need the correct cookies." });
 });
 
 export default router;
